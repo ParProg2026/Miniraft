ff02f2e71a537ce2b032a5d7f0639fb328715d99
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2026-03-27 18:12:17 +0000

    Adapt script

--- start.sh
+++ start.sh
@@ -30,19 +30,19 @@ trap cleanup EXIT
 
 echo "[Cluster Manager] Booting Raft Node 1 ($NODE_1)..."
 # Launch in background (&) and redirect all output to a dedicated log file
-go run ./src/raftserver "$NODE_1" "$PEERS_FILE" > node1_stdout.log 2>&1 &
+go run ./raftserver "$NODE_1" "$PEERS_FILE" > node1_stdout.log 2>&1 &
 
 echo "[Cluster Manager] Booting Raft Node 2 ($NODE_2)..."
-go run ./src/raftserver "$NODE_2" "$PEERS_FILE" > node2_stdout.log 2>&1 &
+go run ./raftserver "$NODE_2" "$PEERS_FILE" > node2_stdout.log 2>&1 &
 
 echo "[Cluster Manager] Booting Raft Node 3 ($NODE_3)..."
-go run ./src/raftserver "$NODE_3" "$PEERS_FILE" > node3_stdout.log 2>&1 &
+go run ./raftserver "$NODE_3" "$PEERS_FILE" > node3_stdout.log 2>&1 &
 
 echo "[Cluster Manager] Booting Raft Node 4 ($NODE_3)..."
-go run ./src/raftserver "$NODE_4" "$PEERS_FILE" > node4_stdout.log 2>&1 &
+go run ./raftserver "$NODE_4" "$PEERS_FILE" > node4_stdout.log 2>&1 &
 
 echo "[Cluster Manager] Booting Raft Node 5 ($NODE_3)..."
-go run ./src/raftserver "$NODE_5" "$PEERS_FILE" > node5_stdout.log 2>&1 &
+go run ./raftserver "$NODE_5" "$PEERS_FILE" > node5_stdout.log 2>&1 &
 
 echo "[Cluster Manager] Waiting 3 seconds for the cluster to hold a Leader Election..."
 sleep 3
@@ -54,4 +54,4 @@ echo "--------------------------------------------------------------------------
 # Launch the interactive client in the foreground, connecting to Node 1.
 # Since it runs in the foreground, blocking the script, typing 'exit' here
 # will end the script and trigger the cleanup trap.
-go run ./src/raftclient "$NODE_1"
+go run ./raftclient "$NODE_1"
